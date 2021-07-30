b4cb9106b7f26b22dad5d9d4de154101dc787b50
Juliano <julianoomc@gmail.com>
2021-07-30 09:26:49 -0300

    First and last commit

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+
+  items = this.cartService.getItems();
+
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn("Seu pedido foi enviado", this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
+
+}
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../cart.service';
+import { Product, products } from '../products';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
+
+  ngOnInit() {
+    //Pegando o id do produto da rota atual
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    //Procurando o produto que corresponde ao id encontrado
+    this.product = products.find(product => product.id === productIdFromRoute);
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Seu produto foi adicionado ao carrinho!');
+  }
+}
